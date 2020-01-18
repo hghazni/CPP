a3173084cd24cb50952010360cec06ea57369296
Harry Ghazni <hghazni@gmail.com>
2020-01-18 21:38:03 +0000

    Hello World Logic

---
+++ helloWorld.cpp
@@ -0,0 +1,12 @@
+/* helloWorld.cpp */
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Please enter your name: ";
+string name;
+    cin >> name;
+    cout << "Hello, " << name;
+    cout << "\n";
+return 0;
+}
